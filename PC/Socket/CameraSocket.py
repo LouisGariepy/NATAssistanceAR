503ed87b0236830d51c3fd353b14c1c4df077d64
dshdid <dona.shdid97@gmail.com>
2021-04-02 16:17:58 +0100

    :snowflake: refactoring CameraSocket v2
    added constructor
    refactored try except in getFrame (Not tested)

--- PC/Socket/CameraSocket.py
+++ PC/Socket/CameraSocket.py
@@ -2,11 +2,9 @@
     Author: Anthony Melin
     Date: 2019 August 14
 """
-
 from UdpConnection import UdpConnection
 import numpy as np
 import cv2
-
 
 """
 ###############################################################
@@ -22,33 +20,35 @@ import cv2
 ###############################################################
 """
 
+
 class CameraSocket(UdpConnection):
     """
     Inherited from UdpConnection.
     Udp server for asking data in byte format to connected client.
     Receive frames as data and make sure it's readable
     """
-    
-    data = b""
-    data_readable = False
-    
+
+    def __init__(self):
+        super().__init__()
+        self.data = b""
+        self.data_readable = False
+        self.header = self.recvHeader()
+
     def askData(self):
         """
         Request data to the client.
         data_readable indicate if received data are valid. If so, it's stored in data
         """
-        
+
         if not self.IsConnected():
             self.echo("Request not possible: client not connected")
             return
-            
+
         self.echo("Send request for data to client")
         self.sendto(b"\xff", self.client)
-        
-        self.header = self.RecvHeader()
-        if self.header:
-            self.data_readable, self.data = self.RecvPackets()
 
+        if self.header:
+            self.data_readable, self.data = self.recvPackets()
 
     def recvHeader(self):
         """
@@ -56,47 +56,46 @@ class CameraSocket(UdpConnection):
         Return the result of FormatHeader if received the header otherwise return false
         """
 
-        size, header = self.WaitMsg(32, 1, "header") # wait the header
-        
+        size, header = self.WaitMsg(32, 1, "header")  # wait the header
+
         # if received
         if size > 0:
             self.echo("Received header")
             return self.formatHeader(header)
-        
+
         return False
-    
-    
+
     def recvPackets(self):
         """
         Method that receive all packets from client en gather it to restore data
         return a tuple like (data valid as boolean, data as byte array)
         """
-        
+
         data = b""
-        
+
         # loop as many as packet to receive
         for _ in range(self.header["packet"]):
-            
-            self.sendto(b"\xfe", self.client) # send message for next packet
-            received, packet = self.WaitMsg(65000, 1) # wait the packet
-            
-            if not received: return False, self.data # in case of timeout or error
-            else: data += packet # concat the packet to data
-        
+
+            self.sendto(b"\xfe", self.client)  # send message for next packet
+            received, packet = self.WaitMsg(65000, 1)  # wait the packet
+
+            if not received:
+                return False, self.data  # in case of timeout or error
+            else:
+                data += packet  # concat the packet to data
+
         return True, data
-    
-    
+
     def formatHeader(self, header):
         """
         Method for specify how to read the header. Return a dictionnary containing informations 
         Extract the number of packet to receive from the header.
         """
-        
-        packet = header[0] # packet is the 1st byte received. It's implicitly converted to int
+
+        packet = header[0]  # packet is the 1st byte received. It's implicitly converted to int
         self.echo("packet: {}".format(packet))
-            
-        return {"packet":packet}
-        
+
+        return {"packet": packet}
 
     def getFrame(self):
         """
@@ -104,20 +103,16 @@ class CameraSocket(UdpConnection):
         Frame is returned as numpy array. All values are set to 0 if image is not valid. 
         """
 
+        frame = None
         self.askData()
 
-        #TODO: refactor try except
+        # TODO: refactor try except not tested
         # try to decode frame
         try:
-            frame = cv2.imdecode(np.frombuffer(self.data, np.uint8), -1) # decode the frame
-            
-            # in case of error
-            if frame is None:
-                self.ClearReception()
-                frame = np.zeros((720,1280,3)) # empty array
-        except:
+            frame = cv2.imdecode(np.frombuffer(self.data, np.uint8), -1)  # decode the frame
+
+        except frame is None:
             self.ClearReception()
-            frame = np.zeros((720,1280,3))
+            frame = np.zeros((720, 1280, 3))
 
         return frame
-    
