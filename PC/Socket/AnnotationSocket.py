ddfae83876c0a3c3e62153e42e887492b522220a
dshdid <dona.shdid97@gmail.com>
2021-04-22 13:57:15 +0200

    :snowflake: Done refacotring AnnotationSocket

--- PC/Socket/AnnotationSocket.py
+++ PC/Socket/AnnotationSocket.py
@@ -3,7 +3,7 @@
     Date: 2019 August 14
 """
 
-## @package AnnotationSocket
+# @package AnnotationSocket
 
 # for importation from outside this directory
 from UdpConnection import UdpConnection
@@ -16,15 +16,17 @@ from UdpConnection import UdpConnection
 #                                                             #
 ###############################################################
 """
+
+
 class AnnotationSocket(UdpConnection):
-    
+
     """
     Module defining a specific socket for sending annotation command
 
     Inherited from UdpConnection.
     Specific socket for sending display command
     """
-    
+
     def Draw(self, cmd, *args):
         """[summary]
         Send the command and a vector to client
@@ -33,11 +35,11 @@ class AnnotationSocket(UdpConnection):
             args ([float]): optionnal
 
         """
-        
+
         if not self.IsConnected():
             self.echo("Request not possible: client not connected")
-            return 
-        
+            return
+
         cmd += ";"
 
         # add each optional argument
