b06d2cdc822d0a90651963794f6ceb33232c2110
dshdid <dona.shdid97@gmail.com>
2021-04-22 11:27:47 +0200

    :snowflake: Refactoring AnntationSocket
    removed unnecessary variable message
    added comments

--- PC/Socket/AnnotationSocket.py
+++ PC/Socket/AnnotationSocket.py
@@ -4,49 +4,46 @@
 """
 
 ## @package AnnotationSocket
-# Module defining a specific socket for sending annotation command
 
-# coding: utf-8
-
-try:
-    # for importation from outside this directory
-    from Socket.UdpConnection import UdpConnection
-except:
-    # for importation from here
-    from UdpConnection import UdpConnection
+# for importation from outside this directory
+from UdpConnection import UdpConnection
 
 """
 ###############################################################
 #                     AnnotationSocket                        #
 #                                                             #
 #  Draw(self, cmd, vector, *args)                             #
-#  Clear(self)                                                #
 #                                                             #
 ###############################################################
 """
-## Inherited from UdpConnection.
-# Specific socket for sending display command
 class AnnotationSocket(UdpConnection):
     
+    """
+    Module defining a specific socket for sending annotation command
+
+    Inherited from UdpConnection.
+    Specific socket for sending display command
+    """
     
-    """###########################################################"""
-    ## Send the command and a vector to client
-    # @param cmd string, command for displaying
-    # @param vector tuple representing the 3D coordinates of what the command call to display, values (x, y, z) may be float
-    # @param *args optional args, must be a type that could be converted to float
     def Draw(self, cmd, *args):
+        """[summary]
+        Send the command and a vector to client
+        Args:
+            cmd ([string]): command for displaying
+            args ([float]): optionnal
+
+        """
         
         if not self.IsConnected():
             self.echo("Request not possible: client not connected")
-            
-        else:
-            # add command to message
-            message = cmd+";"
+            return 
+        
+        cmd += ";"
 
-            # add each optional argument
-            for arg in args:
-                message += str(arg)+";"
+        # add each optional argument
+        for arg in args:
+            cmd += str(arg)+";"
 
-            # send command and wait a message that indicate the command is applied
-            self.sendto(message.encode(), self.client)
-            self.WaitMsg(32, 1)
+        # send command and wait a message that indicate the command is applied
+        self.sendto(cmd.encode(), self.client)
+        self.WaitMsg(32, 1)
